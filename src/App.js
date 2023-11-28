037bcaabd6a2674fb22f8e79c9e4f432e4776141
Amod Nagpal <amodnagpal46@gmail.com>
2023-11-28 14:31:37 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Temp from "./temp";
+
+export default function App() {
+  return (
+    <section>
+      <Temp />
+    </section>
+  );
+}
---
+++ src/xyz.js
@@ -0,0 +1,5 @@
+function abc()
+{
+    <h1>Hello</h1>
+}
+export default abc()
